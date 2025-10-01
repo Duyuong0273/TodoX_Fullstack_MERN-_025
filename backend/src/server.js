0c6f2086b7b9d238a99e7133b72e66c2e09805d7
Duyuong0273 <duyuong0273@gmail.com>
2025-10-02 00:48:27 +0700

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,37 @@
-import express from 'express'
-import tasksRoute from './routes/tasksRouters.js'
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import tasksRoute from "./routes/tasksRouters.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 const app = express();
 
-
 //middlewares
-app.use(express.json())
-app.use(cors({origin:"http://localhost:5173"}))
+app.use(express.json());
 
-app.use("/api/tasks", tasksRoute)
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({ origin: "http://localhost:5173" }));
 
+}
 
+app.use("/api/tasks", tasksRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/todoX/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/todoX/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server bắt đầu trên cổng ${PORT}`);
-    })
-    
+  app.listen(PORT, () => {
+    console.log(`Server bắt đầu trên cổng ${PORT}`);
+  });
 });
-
--- frontend/todoX/src/lib/axios.js
+++ frontend/todoX/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BASE__URL=import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+
 const api = axios.create({
-    baseURL:"http://localhost:5001/api"
+    baseURL: BASE__URL
 })
 export default api
